70bef29856c9f5074e216831831527bb2815e611
rahulrajvn <rahulrajvn@gmail.com>
2021-02-01 23:30:52 +0400

    Create Untitled1.ps1

---
+++ Untitled1.ps1
@@ -0,0 +1,7 @@
+﻿Netsh trace start scenario=netconnection capture=yes tracefile=c:\client_nettrace.etl maxsize=512 overwrite=yes report=yes
+
+
+
+Netsh trace stop
+
+Portqry.exe -n 127.0.0.1 -e 135
