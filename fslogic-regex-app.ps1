3eb2b73b781f98becd82720b7b616912b47d6ebd
rahulrajvn <rahulrajvn@gmail.com>
2021-03-26 00:02:07 +0400

    Update fslogic-regex-app.ps1

--- fslogic-regex-app.ps1
+++ fslogic-regex-app.ps1
@@ -1,13 +1,15 @@
 
 
 
-
+https://rdweb.wvd.microsoft.com/arm/webclient/index.html
 
 cd HKLM:\
 Get-childitem	
 set-location -path 'HKLM:\SOFTWARE\FSLogix\Profiles'
 Get-childitem
-New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VolumeType" -Value "vhdx" -PropertyType "MultiString" -Froce
+
+# Enable FSlogix -Forcefully
+New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "VHDLocations" -Value "\\wvdadf.file.core.windows.net\wvdadfshare" -PropertyType "MultiString" -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "Enabled" -Value "1"  -PropertyType "DWord" -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "DeleteLocalProfileWhenVHDShouldApply" -Value "1"  -PropertyType "DWord" -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "FlipFlopProfileDirectoryName" -Value "1"  -PropertyType "DWord" -Force
@@ -15,17 +17,23 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "PreventLoginWith
 New-ItemProperty -Path "HKLM:\SOFTWARE\FSLogix\Profiles" -Name "PreventLoginWithTempProfile" -Value "1"  -PropertyType "DWord" -Force
 
 
-CCDLocations
-
-Set-ItemProperty -Path 'HKLM\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -Value '0'
-
-\\adfwvd.file.core.windows.net\adfsharewvd
 
 
-set-location -path 'HKLM:\SOFTWARE\FSLogix\Profiles'
+#Disable Fslogic 
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -Value '0'
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'DeleteLocalProfileWhenVHDShouldApply' -Value '0'
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'FlipFlopProfileDirectoryName' -Value '0'
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'PreventLoginWithFailure' -Value '0'
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\FSLogix\Profiles' -Name 'PreventLoginWithTempProfile' -Value '0'
+
+
+#Fslogix Log Path
+%ProgramData%\FSLogix\Logs
+ 
+
+#On Command line not Powershell
+icacls y: /grant rahul@adfolks.com:(M) 
+icacls y: /grant "Creator Owner":(OI)(CI)(IO)(M) 
+icacls y: /remove "Authenticated Users" 
+icacls y: /remove "Builtin\Users"
 
